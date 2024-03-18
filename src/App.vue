10c583146b0de7bd1e66341be206a0c2ca177347
Abhishek <abhitiku2003@gmail.com>
2024-03-18 20:35:50 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline flex w-screen h-screen items-center justify-center bg-gray-700">
+    Hello world!
+  </h1>
+</template>
